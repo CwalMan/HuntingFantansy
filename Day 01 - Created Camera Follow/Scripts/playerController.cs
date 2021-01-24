010c170b1090e36fbd8ad7397f558dbacbeafbf3
CwalMan <75854244+CwalMan@users.noreply.github.com>
2021-01-24 15:48:16 +0900

    Add files via upload

---
+++ Day 01 - Created Camera Follow/Scripts/playerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Vector2 moveInput;
+    public float moveSpd = 4f;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        playerMove();
+    }
+    void playerMove()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        moveInput.Normalize();
+        rb.velocity = moveInput * moveSpd;
+    }
+}
